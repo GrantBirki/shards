7405ecb05cc8d82ad971c7052fa0fd846cf5b2dc
GrantBirki <grant.birkinbine@gmail.com>
2025-01-23 21:03:47 -0800

    use a shallow clone with a fetch depth of 1 for the git resolver

--- src/resolvers/git.cr
+++ src/resolvers/git.cr
@@ -328,13 +328,13 @@ module Shards
       # This configuration can be overridden by defining the environment
       # variable `GIT_ASKPASS`.
       git_retry(err: "Failed to clone #{git_url}") do
-        run_in_folder "git clone -c core.askPass=true -c init.templateDir= --mirror --quiet -- #{Process.quote(git_url)} #{Process.quote(local_path)}"
+        run_in_folder "git clone -c core.askPass=true -c init.templateDir= --mirror --quiet --depth 1 -- #{Process.quote(git_url)} #{Process.quote(local_path)}"
       end
     end
 
     private def fetch_repository
       git_retry(err: "Failed to update #{git_url}") do
-        run "git fetch --all --quiet"
+        run "git fetch --all --quiet --depth 1"
       end
     end
 
